af5f58c3a45809e2e89868f7c57637418ef7dbbf
eaclumpkens <eaclumpkens@gmail.com>
2020-09-12 17:22:23 -0400

    answer functionality

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 var backStatus = quizBody.dataset.status;
+
+var questionNum = document.querySelector("#question-number");
 var questionEl = document.querySelector("#question");
-var answer1 = document.querySelector("#answer-1");
-var answer2 = document.querySelector("#answer-2");
-var answer3 = document.querySelector("#answer-3");
-var answer4 = document.querySelector("#answer-4");
+var answerEl = document.querySelector('#answer-container');
 
 var score = 0;
 var currentQuestion = 0;
@@ -14,59 +13,78 @@ var secondsDisplay = document.querySelector("#seconds");
 var totalSeconds = 60;
 var secondsElapsed = 0;
 
-quizBackground();
 
-var q1 = {
-    "question":"What does 'DOM' stand for?",
-    "a1": "Document Object Model",
-    "a2": "Direct Object Model",
-    "a3": "Document Obstruct Method",
-    "a4": "Document Object Method",
-    "correct": "a1"
-}
+var questions = [
+    {
+        "question":"What does 'DOM' stand for?",
+        "choices": ["Document Object Model","Direct Object Mode","Document Obstruct Method","Document Object Method"],
+        "correct": "Document Object Model"
+    }, 
+    {
+        "question":"Which is NOT considered to be one of the three major front-end programming languages?",
+        "choices": ["JavaScript", "HTML", "Python","CSS"],
+        "correct":"Python"
+    }, 
+    {
+        "question":"Which is NOT one of GitHub's major functions?",
+        "choices": ["Version Control","Code Editor","Collaboration", "Professional Portfolio"],
+        "correct": "Code Editor"
+    }];
 
-var q2 = {
-    "question":"Which is NOT considered to be one of the three major front-end programming languages?",
-    "a1": "JavaScript",
-    "a2": "HTML",
-    "a3":"Python",
-    "a4": "CSS",
-    "correct":"a3"
-}
 
-var q3 = {
-    "question":"Which is NOT one of GitHub's major functions?",
-    "a1": "Version Control",
-    "a2": "Code Editor",
-    "a3": "Collaboration",
-    "a4": "Professional Portfolio",
-    "correct": "a2"
-}
 
-var questions = [q1, q2, q3];
+
+
 
 // QUIZ FUNCTIONALITY
 
 function renderQuestion() {
-    
-    questionEl.textContent = questions[currentQuestion]["question"];
-    answer1.textContent = questions[currentQuestion]["a1"];
-    answer2.textContent = questions[currentQuestion]["a2"];
-    answer3.textContent = questions[currentQuestion]["a3"];
-    answer4.textContent = questions[currentQuestion]["a4"];
+    var activeQuestion = questions[currentQuestion];  
 
+    questionNum.textContent = `Question ${currentQuestion + 1}`;
+    questionEl.textContent = activeQuestion["question"];
+    
+    for (var i = 0; i < activeQuestion.choices.length; i++) {
+        if (currentQuestion === 0) {
+            var newBut = document.createElement("button");
+
+            newBut.classList.add("answer", "btn", "btn-choice", "col-md-12");
+            newBut.setAttribute("id", `answer-${i}`)
+            newBut.textContent = activeQuestion.choices[i];
+            answerEl.append(newBut);
+        } else {
+            answerEl.children[i].textContent = activeQuestion.choices[i];
+        }    
+    }
+    
 }
 
-function startQuiz() {
-    renderQuestion();
-    startTimer();
+function nextQuestion() {
+    if (currentQuestion < questions.length-1) {
+        currentQuestion++;
+        renderQuestion();
+    } else {
+        gameOver();
+    }
+}
+
+function checkAnswer(answerChoice, correctAnswer) {
+
+    if (correctAnswer !== answerChoice) {
+        totalSeconds -=5;  
+        
+    } else {
+        score++;
+    }
+    console.log(score);
+    nextQuestion();
 }
 
 function quizBackground() {
     var color1 = "#D79922"
     var color2 = "#EFE2BA"
 
-    if (backStatus == "start") {
+    if (backStatus == "home") {
         document.body.style.background = color1;
         document.body.style.color = color2; 
     } if (backStatus == "quiz") {
@@ -75,6 +93,12 @@ function quizBackground() {
     };
 }
 
+function startQuiz() {
+    renderQuestion();
+    startTimer();
+}
+
+quizBackground();
 startQuiz();
 
 // TIMER FUNCTIONALITY
@@ -103,24 +127,38 @@ function setMinutes() {
     }
     return formattedSeconds;
   }
+
+  function gameOver() {
+
+  }
   
   function renderTime() {
     minutesDisplay.textContent = setMinutes();
     secondsDisplay.textContent = setSeconds();
   
     if (secondsElapsed >= totalSeconds) {
-        alert("Time's Up!");
-        stopTimer();
+        // alert("Time's Up!");
+        gameOver();
     }
   }
   
   function startTimer() {
     if (totalSeconds > 0) {
-  
       interval = setInterval(function () {
         secondsElapsed++;
         renderTime();
       }, 1000)
-  
-    } 
+    } else {
+        gameOver();
+    }
   }
+
+//   EVENT LISTENERS
+
+answerEl.addEventListener("click", function(event) {   
+    var answerChoice = event.target.textContent
+    var correctAnswer = questions[currentQuestion]["correct"];
+
+    console.log(answerChoice);
+    checkAnswer(answerChoice, correctAnswer);
+})
